ae260b9ffb562253fed5efeb237c2acd3f9c2b69
intaozolina <inta.ozolina@gmail.com>
2022-04-26 08:28:50 +0300

    added buttons with Mathematical counts, added 2 states

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [showReset, setShowReset] = useState(true);
 
+  const buttonDetails = [
+    {
+      name: 'Add 1',
+      clickHandler: () => { setCount(count + 1); },
+    },
+    {
+      name: 'Remove 1',
+      clickHandler: () => { setCount(count - 1); },
+    },
+    {
+      name: 'Multiply by 2',
+      clickHandler: () => { setCount(count * 2); },
+    },
+    {
+      name: 'Divide by 2',
+      clickHandler: () => { setCount(count / 2); },
+    },
+    {
+      name: 'Clear',
+      clickHandler: () => { setCount(0); },
+    },
+  ];
+  const countLastNum = () => {
+    const lastNum = count.toString().slice(-1);
+    return lastNum === '7';
+  };
+
+  return (
+    <div className="App">
+      <h1 style={countLastNum() ? { color: 'gold' } : { color: 'inherit' }}>
+        {count}
+      </h1>
+      {buttonDetails
+        .map(({ name, clickHandler }, index) => (
+          <button
+            key={Math.random()}
+            onClick={() => { clickHandler(); }}
+          >
+            {name}
+          </button>
+        ))}
+      <button
+        className="button"
+        key={Math.random()}
+        onClick={() => { setShowReset(!showReset); }}
+      >
+        {showReset ? 'Hide reset' : 'Show reset'}
+      </button>
+      {showReset && (
+      <button className=" button" key={Math.random()} onClick={() => setCount(0)}>
+        Reset
+      </button>
+      )}
+    </div>
+  );
+};
 export default App;
