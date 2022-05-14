1eb0fbd9eddac1fc1b06682a79bcdf7b668e2be1
intaozolina <inta.ozolina@gmail.com>
2022-05-14 14:09:46 +0300

    Added first redux state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import './App.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement, increment, incrementByAmount, multiply, divide, reset,
+} from './components/Counter/counterSlice';
+import { AppDispatch, RootState } from './store';
 
 const App = () => {
+  const count = useSelector((state:RootState) => state.counter.count);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
-
-  )
-    }
-;
-
+    <div>
+      <div style={{ padding: '50px' }}>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <h1 style={{ fontSize: '50px' }}>{count}</h1>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment by 5
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(multiply())}
+        >
+          Multiply by 7
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(divide())}
+        >
+          Divide by 2
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(reset(0))}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
 export default App;
---
+++ src/components/Counter/counterSlice.tsx
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    count: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+    multiply: (state) => {
+      state.count *= 7;
+    },
+    divide: (state) => {
+      state.count /= 2;
+    },
+    reset: (state, action) => {
+      state.count = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {
+  increment, decrement, incrementByAmount, multiply, divide, reset,
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
